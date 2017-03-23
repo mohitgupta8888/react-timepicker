5b6a32c28e28bb567178b754874240450d21ac90
Mohit Gupta <mohitgupta8888@gmail.com>
2017-03-23 16:20:09 +0530

    Added index.js

---
+++ dist/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _TimePicker = require('./TimePicker');
+
+var _TimePicker2 = _interopRequireDefault(_TimePicker);
+
+var _TimePickerInput = require('./TimePickerInput');
+
+var _TimePickerInput2 = _interopRequireDefault(_TimePickerInput);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = { TimePicker: _TimePicker2.default, TimePickerInput: _TimePickerInput2.default };
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import TimePicker from './TimePicker'
+import TimePickerInput from './TimePickerInput'
+
+export default {TimePicker, TimePickerInput}
