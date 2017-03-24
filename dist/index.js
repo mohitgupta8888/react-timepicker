d691bd0a59be41099f7d7672c9812497939f0077
Mohit Gupta <mohitgupta8888@gmail.com>
2017-03-24 10:28:57 +0530

    Built export changes.

--- dist/index.js
+++ dist/index.js
@@ -3,6 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+exports.TimePickerInput = exports.TimePicker = undefined;
 
 var _TimePicker = require('./TimePicker');
 
@@ -14,4 +15,5 @@ var _TimePickerInput2 = _interopRequireDefault(_TimePickerInput);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-exports.default = { TimePicker: _TimePicker2.default, TimePickerInput: _TimePickerInput2.default };
+exports.TimePicker = _TimePicker2.default;
+exports.TimePickerInput = _TimePickerInput2.default;
